2be2a0b8544a95f5bcfcb657cd92792e16300f62
Shist <shist.pupust@mail.ru>
2021-01-11 11:22:15 +0300

    Added project with calculator for DS of banker

---
+++ Development of client-server applications (DS)/RKSBP_Home_Task_1/main.py
@@ -0,0 +1,8 @@
+num1 = 123456789.1
+num2 = 123456787.1
+diff1 = num1 - num2
+print("diff1 = ", diff1)
+num3 = 87654321098765432.1
+num4 = 87654321098765431.1
+diff2 = num3 - num4
+print("diff2 = ", diff2)
