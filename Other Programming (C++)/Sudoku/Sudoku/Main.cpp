93966dfe1289ace05e08d66bdc827314be2ac1d5
Shist <shist.pupust@mail.ru>
2022-01-22 15:55:48 +0300

    Refactored some C++ projects...

--- Other Programming (C++)/Sudoku/Sudoku/Main.cpp
+++ Other Programming (C++)/Sudoku/Sudoku/Main.cpp
@@ -1,157 +1,166 @@
-#include <fstream> // для работы со считыванием из Sudoku.in.txt и выводом в Sudoku.out.txt
-#include <iostream> // для того, чтобы выводить сообщения в консоль
+п»ї#include <fstream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std; // пространство имен
+using namespace std;
 
-// Паша, не страдай херью, пожалуйста (с) Глебастер
+int unsolved_count = 81;
 
-bool Everything_OK(vector <vector<int>> kletka, int N) // Эта функция проверяет две вещи...
+bool Everything_OK(vector <vector<int>> kletka, int N)
 {
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			if (kletka[i][j] == 0) // 1) нету ли пустых клеток в получившемся судоку...
+			if (kletka[i][j] == 0)
 				return false;
-			for (int k = 0; k < N; k++) // А также 2) то, ... 
+			for (int k = 0; k < N; k++) 
 			{
 				if (j == k)
 					continue;
-				if (kletka[i][j] == kletka[i][k]) // не повторяются ли числа в строках
+				if (kletka[i][j] == kletka[i][k])
 					return false;
 			}
 			for (int k = 0; k < N; k++)
 			{
 				if (i == k)
 					continue;
-				if (kletka[i][j] == kletka[k][j]) // и не повторяются ли она в столбцах...
+				if (kletka[i][j] == kletka[k][j])
 					return false;
 			}
 		}
 	}
-	return true; // Тогда и только тогда, судоку будет верным и функция вернет TRUE
+	return true;
 }
 
-void Vstavka(vector <vector<int>> &ish_kl, vector<vector<int>> &kl, int N)
+bool FieldDecision(vector<vector<int>>& kl, int start_Y, int start_X)
 {
-	for (int i = 0; i < N; i++)
+	bool something_changed = false;
+	for (int num_for_check = 1; num_for_check < 10; num_for_check++)
 	{
-		for (int j = 0; j < N; j++)
+		vector <vector<char>> field;
+		field.resize(3);
+		for (int i = 0; i < 3; i++)
+			field[i].resize(3);
+		int field_unsolved_count = 9;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				if (kl[start_Y + i][start_X + j] == 0)
+					field[i][j] = 'Y';
+				else
+				{
+					field_unsolved_count--;
+					field[i][j] = 'N';
+				}
+		bool skip_num = false;
+		for (int i = start_Y; i < start_Y + 3; i++)
+			for (int j = start_X; j < start_X + 3; j++)
+				if (kl[i][j] == num_for_check)
+					skip_num = true;
+		if (skip_num)
+			continue;
+		for (int i = start_Y; i < start_Y + 3; i++)
 		{
-			kl[i][j] = ish_kl[i][j];
+			for (int j = 0; j < 9; j++)
+				if (kl[i][j] == num_for_check)
+					for (int f_j = 0; f_j < 3; f_j++)
+						if (field[i - start_Y][f_j] != 'N')
+						{
+							field_unsolved_count--;
+							field[i - start_Y][f_j] = 'N';
+						}
 		}
+		for (int j = start_X; j < start_X + 3; j++)
+		{
+			for (int i = 0; i < 9; i++)
+				if (kl[i][j] == num_for_check)
+					for (int f_i = 0; f_i < 3; f_i++)
+						if (field[f_i][j - start_X] != 'N')
+						{
+							field_unsolved_count--;
+							field[f_i][j - start_X] = 'N';
+						}
+		}
+		if (field_unsolved_count == 1)
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+					if (field[i][j] == 'Y')
+					{
+						something_changed = true;
+						unsolved_count--;
+						kl[start_Y + i][start_X + j] = num_for_check;
+					}
 	}
+	return something_changed;
 }
 
 int main()
 {
-	ifstream fin("Sudoku.in.txt");
-	ofstream fout("Sudoku.out.txt");
-	int N; // Размерность судоку
-	fin >> N; // Считываем здесь размерность судоку... (обычно она равна 9)
-	vector <vector<int>> ishodnaya_kletka;
+	ifstream fin("Sudoku_in.txt");
+	ofstream fout("Sudoku_out.txt");
+	int N;
+	fin >> N;
 	vector <vector<int>> kletka;
-	vector <int> chislo = { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
-	ishodnaya_kletka.resize(N);
 	kletka.resize(N);
 	int i = 0;
 	int j = 0;
 	int num = 0;
 	for (i = 0; i < N; i++)
 	{
-		ishodnaya_kletka[i].resize(N);
 		kletka[i].resize(N);
 		for (j = 0; j < N; j++)
 		{
 			fin >> num;
-			if (num != 0)
-			{
-				chislo[num - 1]--; // Учитываем, сколько и каких цифр в судоку уже есть, а сколько нету...
-			}
-			ishodnaya_kletka[i][j] = num;
 			kletka[i][j] = num;
+			if (num != 0)
+				unsolved_count--;
 		}
 	}
-	vector <int> mas; // В этот массив будем помещать все те цифры, которые не использовались и проходиться по их комбинациям пузырьком
-	for (i = N - 1; i >= 0; i--) // Заполняем этот массив недостающими в судоку цифрами (по убыванию)
+	while (true)
 	{
-		if (chislo[i] != 0)
+		bool nothing_changed = true;
+		if (FieldDecision(kletka, 0, 0))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 0, 3))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 0, 6))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 3, 0))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 3, 3))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 3, 6))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 6, 0))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 6, 3))
+			nothing_changed = false;
+		if (FieldDecision(kletka, 6, 6))
+			nothing_changed = false;
+		if (unsolved_count == 0)
 		{
-			mas.push_back(i + 1);
-			chislo[i]--;
-			i++;
+			fout << "Success" << endl;
+			break;
+		}
+		if (nothing_changed)
+		{
+			fout << "Too difficult, decision is imperfect" << endl;
+			break;
 		}
 	}
-	int check = 0;
-	int k = 0;
 	for (i = 0; i < N; i++)
-	{
 		for (j = 0; j < N; j++)
 		{
-			if (kletka[i][j] == 0)
+			if (j == N - 1)
 			{
-				kletka[i][j] = mas[k];
-				k++;
+				if (i != N - 1)
+					fout << kletka[i][j] << endl;
+				else
+					fout << kletka[i][j];
+				break;
 			}
+			fout << kletka[i][j] << " ";
 		}
-	}
-	fout << "The decision of the sudoku is the next:" << endl;
-	long long g = 0;
-	do
-	{
-		g++;
-		for (i = 0; i < N; i++)
-		{
-			for (j = 0; j < N; j++)
-			{
-				if (kletka[i][j] == 0)
-				{
-					kletka[i][j] = mas[k]; // вставляем в пустые клетки недостающие цифры текущего сочетания
-					k++;
-				}
-			}
-		}
-		if (g % 1000000 == 0)
-		{
-			cout << "###########################################" << endl;
-			cout << "Hello, I'm program, and I'M ALIVE YET!!!" << endl;
-			cout << "The program is trying to find out correct numbers" << endl;
-			cout << "This operation will take at worst (amount of 0 in the sudoku)! of iterations..." << endl;
-			cout << "Now, the program has done " << g << " iterations..." << endl;
-			cout << "###########################################" << endl;
-		}
-		if (Everything_OK(kletka, N)) // Если все окей, то выводим
-		{
-			cout << "THE PROGRAMM HAS FOUNT CORRECT NUMBERS!!! CHECK SUDOKU.OUT.TXT IMMEDIATLY!!!";
-			for (i = 0; i < N; i++)
-			{
-				for (j = 0; j < N; j++)
-				{
-					if (j == N - 1)
-					{
-						if (i != N - 1)
-						{
-							fout << kletka[i][j] << endl;
-						}
-						else
-						{
-							fout << kletka[i][j];
-						}
-						break;
-					}
-					fout << kletka[i][j] << " ";
-				}
-			}
-			goto link;
-		}
-		else
-		{
-			Vstavka(ishodnaya_kletka, kletka, N); // Если не получилось, то возвращаем все, как было
-			k = 0;
-		}
-	} while (prev_permutation(mas.begin(), mas.end()));
-link:
 	fin.close();
 	fout.close();
 	return 0;
@@ -176,3 +185,16 @@ link:
 3 7 5 9 2 6 4 8 1
 8 1 4 7 3 5 2 9 6
 6 9 2 8 1 4 5 3 7*/
+
+/*
+0 3 2 0 5 0 1 0 0
+0 4 0 6 0 2 0 0 7
+1 0 0 7 0 0 5 4 0
+0 0 1 0 9 0 0 7 3
+8 0 6 0 0 3 2 0 0
+0 7 0 8 4 0 0 9 0
+3 0 0 9 6 0 0 0 8
+0 8 4 0 0 7 0 0 1
+5 0 0 0 0 4 7 3 0*/
+/*
+*/
